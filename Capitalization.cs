8f617a6f80e1d9c4b2168f53c8c6ec50869737df
Vrishali Pal <vrishalipal@yahoo.com>
2019-12-19 19:22:45 -0500

    Capitalize every word in a phrase

---
+++ Capitalization.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsCSharp
+{
+    class Capitalization
+    {
+        public static string CapitalizeWords(string phrase)
+        {
+            char[] array = phrase.ToCharArray();
+
+            if(array.Length >= 1 && char.IsLower(array[0]))
+            {
+                array[0] = char.ToUpper(array[0]);
+            }
+            for (var i = 1; i < array.Length; i++)
+            {
+                if (array[i-1] == ' ' && char.IsLower(array[i])) {
+                    array[i] = char.ToUpper(array[i]);
+                }
+            }
+            
+            return new string(array);
+        }
+
+        public static string TitleCaseWords(string phrase)
+        {
+            string titleCase = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(phrase);
+            return titleCase;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -22,13 +22,49 @@ namespace AlgorithmsCSharp
             //Twins.FindAllTwinNumbers(random);
 
             //Run code for checking Anagrams
-            var strings = new List<string>() { "abcd", "acdb", "dabc" };
-            Console.WriteLine(Anagrams.AreAnagrams(strings));
-            var strings1 = new List<string>() { "debit carb", "bad credit" };
-            Console.WriteLine(Anagrams.AreAnagrams(strings1));
+            //var strings = new List<string>() { "abcd", "acdb", "dabc" };
+            //Console.WriteLine(Anagrams.AreAnagrams(strings));
+            //var strings1 = new List<string>() { "debit carb", "bad credit" };
+            //Console.WriteLine(Anagrams.AreAnagrams(strings1));
+
+
+            //Console.WriteLine($"Vowel Count: { GetVowelCount("I am going to the beachside today, it's a beautiful day!")}");
+
+            // Uppercase words in these strings.
+            const string str1 = "something in the way";
+            const string str2 = "dot net PERLS";
+            const string str3 = "String_two;three";
+            const string str4 = " sam";
+            const string str5 = " ";
+
+            Console.WriteLine(Capitalization.CapitalizeWords(str1));
+            Console.WriteLine(Capitalization.CapitalizeWords(str2));
+            Console.WriteLine(Capitalization.CapitalizeWords(str3));
+            Console.WriteLine(Capitalization.CapitalizeWords(str4));
+            Console.WriteLine(Capitalization.CapitalizeWords(str5));
+
+            Console.WriteLine(Capitalization.TitleCaseWords("how Beautiful is the Day!!"));
+
+
 
             Console.WriteLine("Press Enetr to exit");
             Console.ReadLine();
+
+        }
+
+        private static int GetVowelCount(string str)
+        {
+            // linear time complexity and constant space complexity because every character needs to be checked and temporary primitives are negligible.
+            str = str.ToLower();
+            //var vowels = new List<char>() { 'a', 'e', 'i', 'o', 'u' };
+            string vowels = "aeiou";
+            var count = 0;
+            foreach (var character in str)
+            {
+                if (vowels.Contains(character.ToString()))
+                    count++;
+            }
+            return count;
         }
     }
 }
