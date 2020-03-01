acc9180b86219e2c757bc7d2215819029392d842
Vrishali Pal <vrishalipal@yahoo.com>
2020-03-01 13:33:00 -0500

    Code refactored for time and memory optimization

--- FirstDuplicate.cs
+++ FirstDuplicate.cs
@@ -1,30 +1,11 @@
-//find the first duplicate number for which the second occurrence has the minimal index.
-int firstDuplicateNumber(int[] array) 
-{
-    var duplicates = new Dictionary<int, int>();
-    
-    for(int i = 0; i < array.Length; i++ )
-    {
-        if(duplicates.ContainsKey(array[i])) 
+int FirstDuplicate(int[] a)
         {
-            duplicates[array[i]] = i;
+            foreach(var val in a)
+            {
+                if (a[Math.Abs(val) - 1] < 0)
+                    return Math.Abs(val);
+
+                a[Math.Abs(val) - 1] = -a[Math.Abs(val) - 1];
+            }
+            return -1;
         }
-        else 
-        {
-           duplicates.Add(array[i], array.Length);
-        }
-    }
-    
-    int min = array.Length;
-    int minKey = 0;
-    foreach(KeyValuePair<int, int> kvp in duplicates) 
-    {
-        if(kvp.Value < min) {
-            min = kvp.Value;
-            minKey = kvp.Key;
-        }
-    }
-    
-    return min >= array.Length ? -1 : minKey;
-    
-}
