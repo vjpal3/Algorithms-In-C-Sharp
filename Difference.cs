e4e51e862f4648f61acbd3d2fbd9ddc453354388
Vrishali Pal <vrishalipal@yahoo.com>
2020-03-02 20:24:50 -0500

    Find difference between largest even and smallest odd numbers in array

---
+++ Difference.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsCSharp
+{
+    public class Difference
+    {
+        public static int FindDiffLargestEvenSmallestOdd(int[] numbers)
+        {
+            int largestEvenNumber = int.MinValue;
+            int smallestOddNumber = int.MaxValue;
+
+            for(var i = 0; i < numbers.Length; i++)
+            {
+                if(numbers[i] % 2 == 0)
+                {
+                    if (numbers[i] > largestEvenNumber)
+                        largestEvenNumber = numbers[i];
+                }
+                else if(numbers[i] % 2 == 1)
+                {
+                    if (numbers[i] < smallestOddNumber)
+                        smallestOddNumber = numbers[i];
+                }
+            }
+
+            return largestEvenNumber - smallestOddNumber;
+        }
+    }
+}
